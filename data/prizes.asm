0014cb4f5eb3ccc3c14eb3aa334db0f90c4328e3
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-23 15:34:28 -0500

    Update prizes.asm

--- data/prizes.asm
+++ data/prizes.asm
@@ -12,53 +12,34 @@ NoThanksText: ; 5284f (14:684f)
 	db "NO THANKS@"
 
 PrizeMenuMon1Entries: ; 52859 (14:6859)
-	db ABRA
-	db STARYU
-	db LICKITUNG
+	db BULBASAUR
+	db CHARMANDER
+	db SQUIRTLE
 	db "@"
 PrizeMenuMon1Cost: ; 5285d (14:685d)
-IF DEF(_RED)
-	db $01,$80
-	db $05,$00
-ENDC
-IF DEF(_BLUE)
-	db $01,$20
-	db $07,$50
-ENDC
-	db $12,$00
+	db $10,$00 ; BULBASAUR L20, 20k pen
+	db $10,$00 ; CHARMANDER L20, 20k pen
+	db $10,$00 ; SQUIRTLE L20, 20k pen
 	db "@"
 
 PrizeMenuMon2Entries: ; 52864 (14:6864)
-IF DEF(_RED)
+	db EEVEE
 	db DRATINI
-	db SCYTHER
-ENDC
-IF DEF(_BLUE)
-	db PINSIR
-	db DRATINI
-ENDC
 	db PORYGON
 	db "@"
 PrizeMenuMon2Cost: ; 52868 (14:6868)
-IF DEF(_RED)
-	db $28,$00
-	db $55,$00
-	db $99,$99
-ENDC
-IF DEF(_BLUE)
-	db $25,$00
-	db $46,$00
-	db $65,$00
-ENDC
+	db $25,$00 ; SCYTHER L30, 50k pen
+	db $25,$00 ; PINSIR L30, 50k pen
+	db $99,$99 ; PORYGON L30, 200k pen
 	db "@"
 
 PrizeMenuTMsEntries: ; 5286f (14:686f)
-	db TM_23
-	db TM_15
-	db TM_50
+	db NUGGET
+	db HELIX_FOSSIL
+	db DOME_FOSSIL
 	db "@"
 PrizeMenuTMsCost: ; 52873 (14:6873)
-	db $33,$00 ; 3300 Coins
-	db $55,$00 ; 5500 Coins
-	db $77,$00 ; 7700 Coins
+	db $02,$50 ; NUGGET, 5k pen
+	db $25,$00 ; HELIX FOSSIL, 50k pen
+	db $25,$00 ; DOME FOSSIL, 50k pen
 	db "@"
