3b5f4c9a55639dcc3f2831443f64b4e15598942b
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-25 17:42:02 -0500

    Update route3.asm

--- data/wildPokemon/route3.asm
+++ data/wildPokemon/route3.asm
@@ -1,14 +1,14 @@
 Route3Mons:
 	db $14
-	db 8,ABRA
-	db 8,MANKEY
-	db 7,MACHOP
-	db 7,SHELLDER
-	db 5,EEVEE
-	db 8,PSYDUCK
-	db 8,STARYU
-	db 8,HORSEA
-	db 8,VOLTORB
-	db 7,PORYGON
+	db 8,SPEAROW     ; 25% Spearow
+	db 8,PIDGEY      ; 20% Pidgey
+	db 9,MANKEY      ; 15% Mankey
+	db 7,JIGGLYPUFF  ; 15% Sandshrew
+	db 8,SANDSHREW   ; 15% Rattata
+	db 10,RATTATA    ; 10% Jigglypuff
+	db 10,SANDSHREW
+	db 12,RATTATA
+	db 11,SPEAROW
+	db 12,SPEAROW
 	db $00
 
