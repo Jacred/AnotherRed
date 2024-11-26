fbdb192893e77480e88ce02949d8c82767ea466f
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-25 22:02:24 -0500

    Update pokemontower3.asm

--- data/wildPokemon/pokemontower3.asm
+++ data/wildPokemon/pokemontower3.asm
@@ -1,11 +1,11 @@
 TowerMons3:
 	db $0A
-	db 34,GASTLY  ; 85% Gastly
-	db 34,GASTLY  ; 9% Cubone
-	db 34,GASTLY  ; 6% Haunter
-	db 34,GASTLY
-	db 34,GASTLY
-	db 34,GASTLY
+	db 33,GASTLY  ; 85% Gastly L34
+	db 34,GASTLY  ; 9% Cubone L35
+	db 35,GASTLY  ; 6% Haunter L37
+	db 36,GASTLY
+	db 37,GASTLY
+	db 32,GASTLY
 	db 35,CUBONE
 	db 37,HAUNTER
 	db 35,CUBONE
