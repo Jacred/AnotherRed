5d747387d123e6a53380b25fba94192017134893
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-25 20:40:08 -0500

    Update pokemontower4.asm

--- data/wildPokemon/pokemontower4.asm
+++ data/wildPokemon/pokemontower4.asm
@@ -3,12 +3,12 @@ TowerMons4:
 	db 34,GASTLY   ; 81% Gastly
 	db 34,GASTLY   ; 10% Haunter
 	db 34,GASTLY   ; 9% Cubone
+	db 34,GASTLY
+	db 34,GASTLY
 	db 40,HAUNTER
 	db 34,GASTLY
-	db 34,GASTLY
 	db 35,CUBONE
-	db 34,GASTLY
-	db 38,CUBONE
+	db 35,CUBONE
 	db 34,GASTLY
 	db $00
 
