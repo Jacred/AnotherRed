ddae741776677c37df882526357bef944efe0c8d
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-25 20:45:26 -0500

    Update pokemontower7.asm

--- data/wildPokemon/pokemontower7.asm
+++ data/wildPokemon/pokemontower7.asm
@@ -1,14 +1,14 @@
 TowerMons7:
 	db $0f
-	db 61,GASTLY
-	db 57,GASTLY
-	db 59,GASTLY
-	db 56,GASTLY
-	db 60,GASTLY
-	db 58,HAUNTER
-	db 55,CUBONE
-	db 57,CUBONE
-	db 58,HAUNTER
-	db 59,HAUNTER
+	db 38,GASTLY   ; 65% Gastly
+	db 38,GASTLY   ; 20% Haunter
+	db 38,GASTLY   ; 15% Cubone
+	db 38,GASTLY
+	db 45,HAUNTER
+	db 38,CUBONE
+	db 38,CUBONE
+	db 45,HAUNTER
+	db 45,HAUNTER
+	db 45,HAUNTER
 	db $00
 
