9334a8947fe1b22a75ffc09a310005863e77191f
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-25 19:21:07 -0500

    Update mtmoon1.asm

--- data/wildPokemon/mtmoon1.asm
+++ data/wildPokemon/mtmoon1.asm
@@ -1,15 +1,15 @@
 MoonMons1:
 	db $0A
-	db 17,ONIX
-	db 17,GEODUDE
-	db 17,ZUBAT
-	db 17,SANDSHREW
-	db 17,CUBONE
-	db 17,PIKACHU
-	db 17,JIGGLYPUFF
-	db 17,CLEFAIRY
-	db 17,NIDORINA
-	db 17,NIDORINO
+	db 12,ZUBAT     ; 70% Zubat L11-14
+	db 13,ZUBAT     ; 20% Geodude L12-13
+	db 12,GEODUDE   ; 5% Paras L12
+	db 11,ZUBAT     ; 4% Sandshrew L14
+	db 13,ZUBAT     ; 1% Clefairy L14
+	db 14,ZUBAT
+	db 13,GEODUDE
+	db 12,PARAS
+	db 14,SANDSHREW
+	db 14,CLEFAIRY
 	db $00
 	
 	
