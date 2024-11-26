e9b143eb39572ddc56f93c7312d24e0983374530
Jacred <118478922+Jacred@users.noreply.github.com>
2024-11-25 20:11:50 -0500

    Update pokemontower5.asm

--- data/wildPokemon/pokemontower5.asm
+++ data/wildPokemon/pokemontower5.asm
@@ -1,14 +1,14 @@
 TowerMons5:
 	db $0A
-	db 55,HAUNTER
-	db 55,HAUNTER
-	db 55,HAUNTER
-	db 55,GASTLY
-	db 55,GASTLY
-	db 55,GASTLY
-	db 55,HAUNTER
-	db 55,CUBONE
-	db 55,CUBONE
-	db 55,GASTLY
+	db 36,GASTLY  ; 76% Gastly
+	db 36,GASTLY  ; 14% Cubone
+	db 36,GASTLY  ; 10% Haunter
+	db 36,GASTLY
+	db 36,GASTLY
+	db 41,HAUNTER
+	db 37,CUBONE
+	db 37,CUBONE
+	db 38,CUBONE
+	db 36,GASTLY
 	db $00
 
